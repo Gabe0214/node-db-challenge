c6d08effef841a2ab257ce53d9deafef649e7e0d
Gabriel Anguiano <anguianog97@gmail.com>
2020-01-24 08:12:43 -0800

    boiler plate set up done

---
+++ router/router-modal.js
@@ -0,0 +1,12 @@
+const knex = require('knex')
+const knexConfig = require('../knexfile')
+
+const dataBase = knex(knexConfig.development)
+
+
+
+module.exports = {
+    // names of database functions 
+
+    
+}
